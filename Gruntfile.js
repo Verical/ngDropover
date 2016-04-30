e506a4be56aa6b236fde9b280f96692926e7f49d
Kevin James <kevinjamesus86@gmail.com>
2016-04-30 13:33:14 -0700

    shim es5 in PhantomJS
    Tests weren't runnable due to phantom throwing an error when Angular
    tried to inject the ngDropover module before running tests. This fixes
    it.

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,8 +90,9 @@ module.exports = function(grunt) {
                     singleRun: true,
                     browsers: ['PhantomJS'],
                     files: [
-                        'node_modules/jquery/dist/jquery.js',
+                        'node_modules/es5-shim/es5-shim.js',
                         'node_modules/angular/angular.js',
+                        'node_modules/jquery/dist/jquery.js',
                         'node_modules/angular-mocks/angular-mocks.js',
                         'src/dropover.js',
                         'test/spec/*.js'
