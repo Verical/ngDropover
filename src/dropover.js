bb76c59ca2155023ebe126b1d2949dd5ab886420
Kevin James <kevinjamesus86@gmail.com>
2016-04-30 15:06:16 -0700

    pass ppseudoElt value of null to getComputedStyle
    https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
    
    Prior to Gecko 2.0 (Firefox 4 / Thunderbird 3.3 / SeaMonkey 2.1), the
    pseudoElt parameter was required. No other major browser required this
    parameter be specified if null. Gecko has been changed to match the
    behavior of other browsers.

--- src/dropover.js
+++ src/dropover.js
@@ -516,7 +516,7 @@
                 if (el.currentStyle) { //IE
                     return el.currentStyle[cssprop];
                 } else if ($window.getComputedStyle) {
-                    return $window.getComputedStyle(el)[cssprop];
+                    return $window.getComputedStyle(el, null)[cssprop];
                 }
                 // finally try and get inline style
                 return el.style[cssprop];
